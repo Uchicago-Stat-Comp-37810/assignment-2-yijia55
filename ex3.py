9be7a413dacbd36bbc38240b6d3a03bfa6825e6e
Yijia Zhao <43759375+yijia55@users.noreply.github.com>
2018-10-20 12:54:50 -0500

    ex1-ex4 python code

---
+++ ex3.py
@@ -0,0 +1,31 @@
+#print a string
+print("I will now count my chickens:")
+
+#print a string and a number which is the calculation result of 25+ (30/6)
+print("Hens", 25 + 30 / 6)
+
+#a%b give the result of a(mod b),this gives 100-((25*3)%4)
+print("Roosters", 100 - 25 * 3 % 4)
+
+#print a string
+print("Now I will count the eggs:")
+
+#print the result of calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#print a string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#print a logical value
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
